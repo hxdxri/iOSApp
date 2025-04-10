9b15f7bb6ef8c75dc42f311d505651ea8d773a1b
Haidari Alhaidari <vrl968@usask.ca>
2025-04-09 19:44:01 -0600

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by ‌ ‌Haidari on 2025-04-09.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTests/AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppTests.swift
+//  AppTests
+//
+//  Created by ‌ ‌Haidari on 2025-04-09.
+//
+
+import Testing
+
+struct AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
